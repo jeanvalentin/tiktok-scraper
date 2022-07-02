9fbb815ab9921e43a2409c93dad90b10afe5b42c
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-07-02 15:39:11 +0200

    Save account data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import puppeteer from 'puppeteer';
 
 const downloadsPath = 'downloads';
 const account = argv[2];
+const accountData = [];
 account ?? process.exit(console.log('Please supply an account name.'));
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
@@ -19,10 +20,14 @@ for (const link of videoLinks) {
 }
 for (const link of videoLinks) {
     try {
+        const videoData = {};
+        videoData.username = account;
         const videoId = link.replace(/^.*\/(\d+)$/g, (u, v) => v);
         // console.log(videoId);
         const videoPage = await browser.newPage();
         await videoPage.goto(link);
+        console.log(videoData);
+        accountData.push(videoData);
         const videoUrl = await videoPage.$eval(`video`, e => e.getAttribute('src'));
         // console.log(videoUrl);
         await videoPage.close();
@@ -36,4 +41,5 @@ for (const link of videoLinks) {
         console.log(`${link}: failure`);
     }
 }
+writeFileSync(`${downloadsPath}/${account}_data.json`, JSON.stringify(accountData));
 await browser.close();
